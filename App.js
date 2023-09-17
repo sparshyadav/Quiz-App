e011afc6ff3cf3f16399da1839682fac5ba3b423
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-17 09:46:01 +0530

    Working on the Project Back

--- App.js
+++ App.js
@@ -1,70 +1,71 @@
 const questions = [
     {
-        'que': 'What does the abbreviation HTML stand for?',
-        'a': 'HyperText Markup Language.',
-        'b': 'HighText Markup Language',
-        'c': 'HyperText Markdown Language',
-        'd': 'None of the Above',
-        'cor': 'a'
+        question: 'What does the abbreviation HTML stand for?',
+        answerA: 'Hyper Text Markup Language',
+        answerB: 'Hyper Transfer Markup Language', 
+        answerC: 'High Text Markup Language', 
+        answerD: 'Hyper Text Markeup Language'
     },
     {
-        'que': 'What does the abbreviation CSS stand for?',
-        'a': 'Cascading Special Sheets',
-        'b': 'Cascading Style Sheets',
-        'c': 'Color and Style Sheets',
-        'd': 'None of the Above',
-        'cor': 'b'
+        question: 'What does the abbreviation CSS stand for?',
+        answerA: 'Cascading Special Sheets',
+        answerB: 'Cascading Style Sheets', 
+        answerC: 'Color and Style Sheets', 
+        answerD: 'None of the Above'
     },
     {
-        'que': 'How many sizes of headers are available in HTML by default?',
-        'a': '5',
-        'b': '7',
-        'c': '4',
-        'd': '6',
-        'cor': 'd'
+        question: 'How many sizes of headers are available in HTML by default?',
+        answerA: '5',
+        answerB: '7', 
+        answerC: '4', 
+        answerD: '6'
     },
     {
-        'que': 'In how many ways can CSS be written?',
-        'a': '1',
-        'b': '2',
-        'c': '3',
-        'd': '4',
-        'cor': 'c'
+        question: 'In how many ways can CSS be written?',
+        answerA: '1',
+        answerB: '2', 
+        answerC: '3', 
+        answerD: '4'
     },
     {
-        'que': 'Which of the following does not require a closing tag?',
-        'a': '<br>',
-        'b': '<hr>',
-        'c': 'Both A and B',
-        'd': 'None of the Above',
-        'cor': 'c'
+        question: 'Which of the following does not require a closing tag?',
+        answerA: '<br>',
+        answerB: '<hr>', 
+        answerC: 'Both A and B', 
+        answerD: 'None of the Above'
     },
 ];
 
+const questionElement=document.getElementById("question");
+const answer1=document.querySelector(".btn1");
+const answer2=document.querySelector(".btn2");
+const answer3=document.querySelector(".btn3");
+const answer4=document.querySelector(".btn4");
+const nextButton=document.getElementById("next-btn");
+
 let index=0;
-const queBox=document.getElementById("queBox");
-const option1=document.getElementById("option1");
-const option2=document.getElementById("option2");
-const option3=document.getElementById("option3");
-const option4=document.getElementById("option4");
-const loadQuestion=()=>{
-    const data=questions[index];
-    queBox.innerText=`${index+1}. ${data.que}`;
-    option1.innerText=data.a;
-    option2.innerText=data.b;
-    option3.innerText=data.c;
-    option4.innerText=data.d;
-    console.log(data);
+let score=0;
+
+function startQuiz(){
+    index=0;
+    score=0;
+    nextButton.innerHTML="Next";
+    ShowQuestion();
 }
 
-const submitQuiz=()=>{
-    const ans=getAnswer();
-}
-
-const getAnswer=()=>{
+function ShowQuestion(){
+    let currentQuestion=questions[index];
+    let questionNo=index+1;
+    questionElement.innerHTML=questionNo+". "+currentQuestion.question;
+    answer1.innerHTML=currentQuestion.answerA;
+    answer2.innerHTML=currentQuestion.answerB;
+    answer3.innerHTML=currentQuestion.answerC;
+    answer4.innerHTML=currentQuestion.answerD;
     
 }
 
-loadQuestion();
+startQuiz();
+ShowQuestion();
+
 
 
